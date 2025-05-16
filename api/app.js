996a28c25b47f69bfb6b2dd91e27aae222fddb0c
Asyfdzaky <asyifadzakymaulanaaditria@mail.ugm.ac.id>
2025-05-16 17:35:04 +0700

    donee

--- api/app.js
+++ api/app.js
@@ -6,6 +6,6 @@ const sekolahRoutes = require("../routes/Routes");
 app.use(express.json());
 app.use("/api/sekolah", sekolahRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 module.exports = app;
