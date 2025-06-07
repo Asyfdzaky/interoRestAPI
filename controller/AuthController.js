0c93d976b5daaee7f534fd6918aa1a11e78b0068
Asyfdzaky <asyifadzakymaulanaaditria@mail.ugm.ac.id>
2025-06-07 16:05:29 +0700

    fix jwwwt

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,6 +1,7 @@
 const supabase = require("../supabaseClient");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const generateToken = (admin) => {
   return jwt.sign(
